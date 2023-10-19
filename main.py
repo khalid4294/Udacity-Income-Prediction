755cd92468b258051377d3af25f6e5a39394714d
khalid4294 <alolayankhalid@gmail.com.com>
2023-10-19 18:28:49 +0200

    minor fix

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from starter.ml.data import process_data
 
 # setting up dvc
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("chmod +x dvc")
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
