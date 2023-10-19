f9670fbbe74e9baceda38539fbb948366302cac1
khalid4294 <alolayankhalid@gmail.com.com>
2023-10-19 09:56:30 +0200

    added training code

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.linear_model import LogisticRegression
 
 
 # Optional: implement hyperparameter tuning.
@@ -18,7 +19,10 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -44,11 +48,11 @@ def compute_model_metrics(y, preds):
 
 
 def inference(model, X):
-    """ Run model inferences and return the predictions.
+    """Run model inferences and return the predictions.
 
     Inputs
     ------
-    model : ???
+    model : LogisticRegression model
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +61,6 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+
+    preds = model.predict(X)
+    return preds
--- starter/train_model.py
+++ starter/train_model.py
@@ -1,10 +1,15 @@
 # Script to train machine learning model.
-
-from sklearn.model_selection import train_test_split
-
 # Add the necessary imports for the starter code.
 
+import joblib
+import pandas as pd
+from ml.data import process_data
+from ml.model import train_model
+from sklearn.model_selection import train_test_split
+
+
 # Add code to load in the data.
+data = pd.read_csv("data/census_clean.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -20,9 +25,21 @@ cat_features = [
     "native-country",
 ]
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+    train, categorical_features=cat_features, label="labels", training=True
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test,
+    categorical_features=cat_features,
+    label="labels",
+    training=False,
+    encoder=encoder,
+    lb=lb,
+)
 
-# Train and save a model.
+# train model
+model = train_model(X_train, y_train)
+
+# save model
+joblib.dump(model, "./model/linear_regression_model.pkl")
