4bc6242fdb03fedc637efc466262c9ff7e9297fa
khalid4294 <alolayankhalid@gmail.com.com>
2023-10-23 19:05:15 +0000

    updated model cards - fixed tests

--- test_live_api.py
+++ test_live_api.py
@@ -1,25 +1,21 @@
 import requests
 
 data = {
-    "dict_features": [
-        {
-            "age": "32",
-            "workclass": "Private",
-            "fnlgt": "205019",
-            "education": "Assoc-acdm",
-            "education-num": "12",
-            "marital-status": "Never-married",
-            "occupation": "Sales",
-            "relationship": "Not-in-family",
-            "race": "Black",
-            "sex": "Male",
-            "capital-gain": "0",
-            "capital-loss": "0",
-            "hours-per-week": "50",
-            "native-country": "United-States",
-            "labels": "0",
-        }
-    ]
+    "age": 32,
+    "workclass": "Private",
+    "fnlgt": 205019,
+    "education": "Assoc-acdm",
+    "education_num": 12,
+    "marital_status": "Never-married",
+    "occupation": "Sales",
+    "relationship": "Not-in-family",
+    "race": "Black",
+    "sex": "Male",
+    "capital_gain": 0,
+    "capital_loss": 0,
+    "hours_per_week": 50,
+    "native_country": "United-States",
+    "labels": 0,
 }
 
 end_point = "https://udacity-income-prediction-6a6fa37bfd96.herokuapp.com/predict"
